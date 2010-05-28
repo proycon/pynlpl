4cc99147956b5b359b6733fec90bf3af425b212c
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-05-28 13:23:44 +0000

    added
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@4782 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ clients/srilm.cc
@@ -0,0 +1,42 @@
+#include <string>
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+#include "srilm/include/File.h"
+#include "srilm/include/Ngram.h"
+#include "srilm/include/Vocab.h"
+//#include <NgramStats.h>
+#include "srilm/lm/src/NgramStatsLong.cc"
+
+class LanguageModel
+{
+private:
+	Vocab vocab;
+	Ngram model;
+
+public:
+	LanguageModel(const std::string& filename, int order) : model(vocab, order)
+	{
+		File file(filename.c_str(), "r");
+		model.read(file);
+	}
+
+	LogP wordProb(const std::string& context1, const std::string& context2, const std::string& word)
+	{
+		const VocabIndex context[] = {
+			context2 == "__" ? Vocab_None : vocab.getIndex(context2.c_str()),
+			context1 == "__" ? Vocab_None : vocab.getIndex(context1.c_str())
+		};
+
+		return model.wordProb(vocab.getIndex(word.c_str()), context);
+	}
+};
+
+
+BOOST_PYTHON_MODULE(srilm)
+{
+	class_<LanguageModel, boost::noncopyable>("LanguageModel", init<const std::string&, int>())
+		.def("wordProb", &LanguageModel::wordProb)
+	;
+}
