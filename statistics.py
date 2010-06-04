01f15e579e899ab2b8a64cb2ddde92be6b18756a
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-06-04 11:26:42 +0000

    added levenshtein function
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@4886 12f355fe-0486-481a-ad91-c297ab22b4e3

--- statistics.py
+++ statistics.py
@@ -324,3 +324,23 @@ def normalize(numbers, total=1.0):  #from AI: A Modern Appproach
 
 
 
+def levenshtein(s1, s2): #from http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s1:
+        return len(s2)
+ 
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+ 
+    return previous_row[-1]
+
+
+
