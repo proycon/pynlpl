68597607122e87c59dd7091995fa5a6ab9204544
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-06-14 18:22:32 +0000

    fix
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@5087 12f355fe-0486-481a-ad91-c297ab22b4e3

--- lm/client.py
+++ lm/client.py
@@ -7,11 +7,11 @@ class LMClient(object):
 
     def __init__(self,host= "localhost",port=12346):
         self.BUFSIZE = 1024
-        self.socket = socket.socket(AF_INET,SOCK_STREAM) #Create the socket
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Create the socket
         self.socket.settimeout(120) 
         self.socket.connect((host, port)) #Connect to server
 
-    def scoresentence(self, sentence:
+    def scoresentence(self, sentence):
         if isinstance(sentence,list) or isinstance(sentence,tuple):
             sentence = " ".join(sentence)
         self.socket.sendall(sentence+ "\n")
