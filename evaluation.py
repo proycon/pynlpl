dd97e231fad848506f00612478f8c6e36ab0a572
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-06-20 21:38:42 +0000

    new sizefunc
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@5172 12f355fe-0486-481a-ad91-c297ab22b4e3

--- evaluation.py
+++ evaluation.py
@@ -148,7 +148,7 @@ class WPSParamSearch:
         if sizefunc != None:
             self.sizefunc = sizefunc
         else:
-            self.sizefunc = lambda i, maxsize: round(i/10.0 * maxsize)
+            self.sizefunc = lambda i, maxsize: round((maxsize/100.0)*i^2)
 
         #prunefunc should return a number between 0 and 1, indicating how much is pruned. (for example: 0.75 prunes three/fourth of all combinations, retaining only 25%)
         if prunefunc != None:    
