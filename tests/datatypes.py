a83dd84d69f13a0fb5aab5e3db6bf9dc190fd736
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-01-23 13:19:36 +0000

    Adapted tests for fixed length priority queue (all pass)
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@7936 12f355fe-0486-481a-ad91-c297ab22b4e3

--- tests/datatypes.py
+++ tests/datatypes.py
@@ -14,41 +14,57 @@ values = [3,6,6,1,8,2]
 mintomax = sorted(values)
 maxtomin = list(reversed(mintomax))
 
+
 class PriorityQueueTest(unittest.TestCase):
     def test_append_minimized(self):
         """Minimized PriorityQueue"""
         global values
-        pq = PriorityQueue(values, lambda x: x, True,False,False)
+        pq = PriorityQueue(values, lambda x: x, True,0,False,False)
         result = list(iter(pq))
         self.assertEqual(result, mintomax)
 
     def test_append_maximized(self):
         """Maximized PriorityQueue"""
         global values
-        pq = PriorityQueue(values, lambda x: x, False,False,False)
+        pq = PriorityQueue(values, lambda x: x, False,0,False,False)
         result = list(iter(pq))
         self.assertEqual(result, maxtomin)
 
     def test_append_maximized_blockworse(self):
         """Maximized PriorityQueue (with blockworse)"""
         global values
-        pq = PriorityQueue(values, lambda x: x, False,True,False)
+        pq = PriorityQueue(values, lambda x: x, False,0,True,False)
         result = list(iter(pq))
         self.assertEqual(result, [8,6,6,3])
 
     def test_append_maximized_blockworse_blockequal(self):
         """Maximized PriorityQueue (with blockworse + blockequal)"""
         global values
-        pq = PriorityQueue(values, lambda x: x, False,True,True)
+        pq = PriorityQueue(values, lambda x: x, False,0,True,True)
         result = list(iter(pq))
         self.assertEqual(result, [8,6,3])
 
     def test_append_minimized_blockworse(self):
         """Minimized PriorityQueue (with blockworse)"""
         global values
-        pq = PriorityQueue(values, lambda x: x, True,True,False)
+        pq = PriorityQueue(values, lambda x: x, True,0,True,False)
         result = list(iter(pq))
         self.assertEqual(result, [1,3])
+        
+
+    def test_append_minimized_fixedlength(self):
+        """Fixed-length priority queue (min)"""
+        global values
+        pq = PriorityQueue(values, lambda x: x, True,4, False,False)
+        result = list(iter(pq))
+        self.assertEqual(result, mintomax[:4])        
+
+    def test_append_maximized_fixedlength(self):
+        """Fixed-length priority queue (max)"""
+        global values
+        pq = PriorityQueue(values, lambda x: x, False,4,False,False)
+        result = list(iter(pq))
+        self.assertEqual(result, maxtomin[:4])                
 
 
 if __name__ == '__main__':
