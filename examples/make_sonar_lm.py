c6b73b0d0e2c52d732785ad00bb882941e09e85b
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-02-08 16:03:41 +0000

    update
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@8256 12f355fe-0486-481a-ad91-c297ab22b4e3

--- examples/make_sonar_lm.py
+++ examples/make_sonar_lm.py
@@ -12,18 +12,21 @@ os.environ['PYTHONPATH'] = sys.path[0] + '/../..'
 from pynlpl.formats.sonar import Corpus
 from pynlpl.lm.lm import SimpleLanguageModel
 
-#syntax: ./make_sonar_lm.py sonar_dir output_file n
+#syntax: ./make_sonar_lm.py sonar_dir output_file n [category]
 
 outputfile = sys.argv[2]
 
+
+n=3
+restrictcollection=""
 try:
     n = int(sys.argv[3])
 except:
-    n = 3
+    pass
 
 lm = SimpleLanguageModel(n)
 
-for doc in Corpus(sys.argv[1],'tok'):
+for doc in Corpus(sys.argv[1],'tok',restrictcollection):
     for sentence_id, sentence in doc.sentences():
 	print sentence_id
         words = [ word for word, id, pos, lemma in sentence ]
