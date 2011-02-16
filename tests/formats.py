f6bc9129be35fee2c182f84d499248368cef0310
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-02-16 12:10:14 +0000

    fix
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@8351 12f355fe-0486-481a-ad91-c297ab22b4e3

--- tests/formats.py
+++ tests/formats.py
@@ -27,7 +27,7 @@ class TimblTest(unittest.TestCase):
                 self.assertEqual(distance,None)            
                         
 
-    def test1_db(self):
+    def test2_db(self):
         """Timbl - Distribution output"""
         s = StringIO("a c ? c { c 1.00000, d 1.00000 }\na b ? c { c 1.00000 }\na d ? c { c 1.00000, e 1.00000 }")
         for i, (features, referenceclass, predictedclass, distribution, distance) in enumerate(TimblOutput(s)):
@@ -53,7 +53,7 @@ class TimblTest(unittest.TestCase):
                 self.assertEqual(distance,None)         
 
 
-    def test1_dbdi(self):
+    def test3_dbdi(self):
         """Timbl - Distribution + Distance output"""
         s = StringIO("a c ? c { c 1.00000, d 1.00000 }        1.0000000000000\na b ? c { c 1.00000 }        0.0000000000000\na d ? c { c 1.00000, e 1.00000 }        1.0000000000000")
         for i, (features, referenceclass, predictedclass, distribution, distance) in enumerate(TimblOutput(s)):
