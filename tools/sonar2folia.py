542e929ccc19f56a21ebec02515d665c31f1a98e
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-06-06 06:27:47 +0000

    added multi-processing support
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@10423 12f355fe-0486-481a-ad91-c297ab22b4e3

--- tools/sonar2folia.py
+++ tools/sonar2folia.py
@@ -12,21 +12,11 @@ if __name__ == "__main__":
 
 import pynlpl.formats.folia as folia
 import pynlpl.formats.sonar as sonar
+from multiprocessing import Pool, Process
 import datetime
 
-sonardir = sys.argv[1]
-foliadir = sys.argv[2]
-if foliadir[-1] != '/': foliadir += '/'
-try:
-    os.mkdir(foliadir[:-1])
-except:
-    pass
-        
-print "Building index..."
-index = list(sonar.CorpusFiles(sonardir,'pos', "", lambda x: True, True))
 
-print "Processing..."
-for i, filename in enumerate(index):
+def process(i, filename):
     category = os.path.basename(os.path.dirname(filename))
     progress = round((i+1) / float(len(index)) * 100,1)    
     print "#" + str(i+1) + " " + filename + ' ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' +  str(progress) + '%'
@@ -34,7 +24,7 @@ for i, filename in enumerate(index):
         doc = folia.Document(file=filename)
     except:
         print >> sys.stderr,"ERROR loading " + filename
-        continue        
+        return False
     filename = filename.replace(sonardir,'')
     if filename[0] == '/':
         filename = filename[1:]
@@ -49,7 +39,7 @@ for i, filename in enumerate(index):
         pretokdoc = folia.Document(file=sonardir + '/' + filename)
     except:
         print >> sys.stderr,"ERROR loading " + filename
-        continue    
+        return False  
     for p2 in pretokdoc.paragraphs():
         try:
             p = doc[p2.id]        
@@ -69,13 +59,34 @@ for i, filename in enumerate(index):
         print >> sys.stderr,"ERROR saving " + foliadir + filename
     
     try:
-        f = codecs.open(foliadir + category + '.txt','a','utf-8')
-        f.write('#' + os.path.basename(filename) + '\n')
-        f.write(unicode(doc))    
+        f = codecs.open(foliadir + filename.replace('.xml','.tok.txt'),'w','utf-8')
+        f.write(doc)    
         f.close()        
     except:
-        print >> sys.stderr,"ERROR appending to " + foliadir + category + '.txt'
+        print >> sys.stderr,"ERROR saving " + foliadir + filename.replace('.xml','.tok.txt')
 
             
     sys.stdout.flush()
     sys.stderr.flush()
+    return True
+    
+
+
+
+if __name__ == '__main__':    
+    sonardir = sys.argv[1]
+    foliadir = sys.argv[2]
+    threads = int(sys.argv[3])
+    if foliadir[-1] != '/': foliadir += '/'
+    try:
+        os.mkdir(foliadir[:-1])
+    except:
+        pass
+            
+    print "Building index..."
+    index = list(sonar.CorpusFiles(sonardir,'pos', "", lambda x: True, True))
+
+    print "Processing..."
+    p = Pool(threads)
+    p.map(f, enumerate(index) )
+
