fb441c6165730a4e2ee051462fddf17d484a4978
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-09-08 16:04:04 +0000

    added values() method to FrequencyList, Distribution
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@13117 12f355fe-0486-481a-ad91-c297ab22b4e3

--- statistics.py
+++ statistics.py
@@ -61,6 +61,9 @@ class FrequencyList:
                 return type.lower()
             else:
                 return type
+                
+    def values():
+         return self._count.values()
 
     def append(self,tokens):
         """Add a list of tokens to the frequencylist. This method will count them for you."""
@@ -212,7 +215,9 @@ class Distribution:
         else:
             raise Exception("Can't create distribution")
         self._ranked = None
-        
+
+    def values():
+         return self._count.values()        
 
     def _validate(self,type):
         return type
