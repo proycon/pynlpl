6fbcc087503f2e4cde8d33df30eed7641cd37e11
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-09-30 12:13:49 +0000

    fix
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pynlpl@13350 12f355fe-0486-481a-ad91-c297ab22b4e3

--- formats/dutchsemcor.py
+++ formats/dutchsemcor.py
@@ -33,11 +33,12 @@ class WSDSystemOutput(object):
        assert (not word_id in self.data)
        if isinstance(senses, Distribution):
             self.data[word_id] = ( (x,y) for x,y in senses ) #PATCH UNDONE (#TODO: this is a patch, something's not right in Distribution?)
+            self.distances[word_id]=distance
             return
        else:
            assert isinstance(senses, list) and len(senses) >= 1
 
-       self.distances[word_id]=distance
+       self.distances[word_id]=distance        
        
        if len(senses[0]) == 1:
             #not a (sense_id, confidence) tuple! compute equal confidence for all elements automatically:
