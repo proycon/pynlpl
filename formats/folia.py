f3bb59003f8a83d6938a22c1f17fe441ee17ecbc
Maarten van Gompel <proycon@anaproy.nl>
2011-11-08 15:14:09 +0100

    fix

--- formats/folia.py
+++ formats/folia.py
@@ -3262,7 +3262,7 @@ class Document(object):
         
         """
         if text is Text:
-            text = Text(id=self.id + '.text.' + str(len(self.data)+1) )            
+            text = Text(self, id=self.id + '.text.' + str(len(self.data)+1) )            
         else:
             assert isinstance(text, Text)        
         self.data.append(text)
