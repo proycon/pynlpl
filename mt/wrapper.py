cb74cccadf9994800c364dc85c5a9767a55cda19
Maarten van Gompel <proycon@anaproy.nl>
2012-02-07 16:30:44 +0100

    update

--- mt/wrapper.py
+++ mt/wrapper.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import subprocess
+import getopt
 
 class MTWrapper(object):
     
@@ -166,7 +167,53 @@ class MTWrapper(object):
     def build_moses_phrasetable(self):
         pass            
     
+def usage():
+    print >>sys.stderr,"wrapper.py -- MT wrapper - Outputs a MT wrapper script (python)"
+    print >>sys.stderr,"Mandatory Input:"
+    print >>sys.stderr,"\t-n <name>         Name of the corpus [MANDATORY!]"
+    print >>sys.stderr,"\t-s <file>         Corpus in source language"
+    print >>sys.stderr,"\t-t <file>         Corpus in target language"
+    print >>sys.stderr,"\t-S <code>         Source language (iso-639-1 or 3)"
+    print >>sys.stderr,"\t-T <code>         Target language (iso-639-1 or 3)"
+    print >>sys.stderr,"\t-w <dir>          Work directory (by default: current dir)"
+    
+    
 if __name__ == "__main__":        
+    
+    workdir = os.getcwd()
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hs:t:S:T:n:w:")
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    sourcecorpusfile = targetcorpusfile = sourcelang = targetlang = corpusname = workdir = ""
+
+
+    for o, a in opts:
+        if o == '-s':
+            sourcecorpusfile = a
+        elif o == '-t':
+            targetcorpusfile = a
+        elif o == '-S':
+            sourcelang = a        
+        elif o == '-T':
+            targetlang = a        
+        elif o == '-n':
+            corpusname = a
+        elif o == '-w':
+            workdir = a
+        else:
+            usage()
+            sys.exit(0)
+    
+    if not corpusname:
+        usage()
+        sys.exit(2)
+        
+        
     print """#! /usr/bin/env python
 # -*- coding: utf8 -*-#     
     
@@ -174,21 +221,21 @@ from pynlpl.mt.wrapper import MTWrapper
 
 mtwrapper = MTWrapper()
     
-mtwrapper.CORPUSNAME = ""
-mtwrapper.WORKDIR = "./" 
+mtwrapper.CORPUSNAME = "%s"
+mtwrapper.WORKDIR = "%s"
 
-mtwrapper.TRAINSOURCECORPUS = ""
-mtwrapper.TRAINTARGETCORPUS = ""
+mtwrapper.TRAINSOURCECORPUS = "%s"
+mtwrapper.TRAINTARGETCORPUS = "%s"
 
-mtwrapper.SOURCELANG = "" #language code
-mtwrapper.TARGETLANG = "" #language code
+mtwrapper.SOURCELANG = "%s" #language code
+mtwrapper.TARGETLANG = "%s" #language code
 
 mtwrapper.TOKENIZE_SOURCECORPUS = False #Set to true if the corpus is not tokenised yet
 mtwrapper.TOKENIZE_TARGETCORPUS = False #Set to true if the corpus is not tokenised yet
 mtwrapper.BUILD_SRILM_SOURCEMODEL = False
 mtwrapper.BUILD_SRILM_TARGETMODEL = True
-mtwrapper.BUILD_GIZA_WORDALIGNMENT = False
-mtwrapper.BUILD_MOSES_PHRASETABLE = False
+mtwrapper.BUILD_GIZA_WORDALIGNMENT = True
+mtwrapper.BUILD_MOSES_PHRASETABLE = True
 
 #defaults
 #mtwrapper.PATH_UCTO = ""      
@@ -199,5 +246,5 @@ mtwrapper.BUILD_MOSES_PHRASETABLE = False
 #mtwrapper.PATH_MOSES = ""
 
 mtwrapper.start()
-    """    
+    """ % (corpusname, workdir, sourcecorpusfile, targetcorpusfile, sourcelang, targetlang)
 
