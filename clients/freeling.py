b8fde80b6cac381b21195043516723f3b74d467a
Maarten van Gompel <proycon@anaproy.nl>
2013-02-15 21:32:03 +0100

    adapted freeling for client-server mode

--- clients/freeling.py
+++ clients/freeling.py
@@ -15,50 +15,48 @@
 ###############################################################
 
 
-import codecs
-import os.path
-#import os.subprocess
+import socket
 
 class FreeLingClient:
-    def __init__(self, channel, encoding='iso-8859-15'):
+    def __init__(self, host, port, encoding='utf-8', timeout=120.0):
         """Initialise the client, set channel to the path and filename where the server's .in and .out pipes are (without extension)"""
-        if not os.path.exists(channel+".in"):
-            raise Exception("Channel does not exist. Is the FreeLing server started in the specified directory?")
-        self.channel = channel
         self.encoding = encoding
+        self.BUFSIZE = 4096
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.settimeout(timeout)
+        self.socket.connect( (host,int(port)) )
+        self.encoding = encoding        
 
-
-    def sendrecv(self, sourcewords):       
-        """Send data to server and return received result""" 
-        fin = codecs.open(self.channel+".in",'w', self.encoding, 'ignore')
-        if isinstance( sourcewords, list ):
-            fin.write(" ".join(sourcewords)+ "\n")
-        else:
-            fin.write(sourcewords + "\n")
-        fin.close()
-
-        fout = codecs.open(self.channel+".out",'r',self.encoding, 'ignore')
-        output = []
-        while True:
-            line = fout.readline()
-            if not line:
-                   break #EOF
-            else:
-                if line.strip():
-                    output.append(line.strip())
-        fout.close()
-        return output
         
     def process(self, sourcewords):
         """Process a list of words, passing it to the server and realigning the output with the original words"""
-        targetwords = []
-        for i, outputline in enumerate(self.sendrecv(sourcewords)):
-            cols = outputline.split(" ")
-            subwords = cols[0].lower().split("_")
-            if len(cols) > 2: #this seems a bit odd?
-                for word in subwords: #split multiword expressions
-                    targetwords.append( (word, cols[1], cols[2], i, len(subwords) > 1 ) ) #word, lemma, pos, index, multiword?
 
+        if isinstance( sourcewords, list ):
+            self.socket.sendall(u" ".join(sourcewords).encode(self.encoding) +'\r\n')
+        if isinstance( sourcewords, list ):
+            self.socket.sendall(sourcewords.encode(self.encoding) +'\r\n')
+
+        results = []
+        done = False
+        while not done:    
+            data = ""
+            while not data or data[-1] != '\n':
+                moredata = self.socket.recv(self.BUFSIZE)
+                if not moredata: break
+                data += moredata
+                data = unicode(data,self.encoding)
+
+
+            for i, line in enumerate(data.strip(' \t\r\n').split('\n')):
+                if not line.strip():
+                    done = True
+                    break
+                else:
+                    cols = line.split(" ")
+                    subwords = cols[0].lower().split("_")
+                    if len(cols) > 2: #this seems a bit odd?
+                        for word in subwords: #split multiword expressions
+                            results.append( (word, cols[1], cols[2], i, len(subwords) > 1 ) ) #word, lemma, pos, index, multiword?
 
         sourcewords = [ w.lower() for w in sourcewords ]          
 
@@ -67,14 +65,14 @@ class FreeLingClient:
             found = False
             best = 0  
             distance = 999999          
-            for j, (targetword, lemma, pos, index, multiword) in enumerate(targetwords):
+            for j, (targetword, lemma, pos, index, multiword) in enumerate(results):
                 if sourceword == targetword and abs(i-j) < distance:
                     found = True
                     best = j
                     distance = abs(i-j)
 
             if found:
-                alignment.append(targetwords[best])
+                alignment.append(results[best])
             else:                
                 alignment.append((None,None,None,None,False)) #no alignment found
         return alignment
