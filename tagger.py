02e0becf9543139966f5e0c4d2e1c2e24c4a758b
Maarten van Gompel <proycon@anaproy.nl>
2013-02-18 20:43:07 +0100

    fix

--- tagger.py
+++ tagger.py
@@ -27,22 +27,21 @@ class Tagger(object):
      def __init__(self, *args):        
         global WSDDIR
         self.tagger = None
+        self.mode = args[0]
         if args[0] == "file":
             if len(args) != 2:
-                raise Exception("Syntax: file:[filename]")
+                raise Exception("Syntax: file:[filename]")            
             self.tagger = codecs.open(args[1],'r','utf-8') 
         elif args[0] == "frog":
             if len(args) != 3:
                 raise Exception("Syntax: frog:[host]:[port]")
             from pynlpl.clients.frogclient import FrogClient
-            self.mode = "frog"
             port = int(args[2])
             self.tagger = FrogClient(args[1],port)                
         elif args[0] == "freeling":
             if len(args) != 3:
                 raise Exception("Syntax: freeling:[host]:[port]")
             from pynlpl.clients.freeling import FreeLingClient
-            self.mode = "freeling"
             host = args[1]
             port = int(args[2])
             self.tagger = FreeLingClient(host,port)            
@@ -50,14 +49,12 @@ class Tagger(object):
             if len(args) != 1:
                 raise Exception("Syntax: corenlp")
             import corenlp
-            self.mode = "corenlp"
             print >>sys.stderr, "Initialising Stanford Core NLP"
             self.tagger = corenlp.StanfordCoreNLP()
         elif args[0] == 'treetagger':                        
             if not len(args) == 2:
                 raise Exception("Syntax: treetagger:[treetagger-bin]")
             self.tagger = args[1]            
-            self.mode = "treetagger"
         elif args[0] == "durmlex":
             if not len(args) == 2:
                 raise Exception("Syntax: durmlex:[filename]")
