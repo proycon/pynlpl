3a210042c5ad66dba7ee9172e388a043d671bd93
Maarten van Gompel <proycon@anaproy.nl>
2013-05-14 10:28:10 +0200

    update

--- tools/pynlpl-classdecoder.py
+++ tools/pynlpl-classdecoder.py
@@ -19,7 +19,7 @@ def main():
     """
 
     parser = argparse.ArgumentParser(prog="pynlpl-classdecoder", description='This tool converts one or more tokenised plain text files, with one sentence per line, to a more compressed binary format in which each word-type gets assigned a numeric class value.  Instead of plain text files, FoLiA XML files are also supported as valid input. Certain other NLP tools may make use of this format to conserve memory and attain higher performance.')
-    parser.add_argument("-s","--singleoutput", type=str, help="Decode all files to a single file")
+    parser.add_argument("-s","--singleoutput", type=str, help="Decode all files to a single file, set filename to - to decode to standard output")
     parser.add_argument("--encoding",type=str,default="utf-8",help="Encoding of plain-text input files")
     parser.add_argument("-u","--unknown",action='store_true',default=False,help="Unknown classes will be assigned a special 'unknown' class (used with -c)")
 
@@ -32,7 +32,7 @@ def main():
 
     if args.singleoutput:
         print("Decoding all files to " + args.singleoutput+"...", file=sys.stderr)
-        classencoder.encodefile(args.files, args.singleoutput, args.encoding)
+        classencoder.encodefile(args.files, args.singleoutput if args.singleoutput != '-' else None, args.encoding)
     else:
         for filename in args.files:
             targetfile = os.path.basename(filename).replace('.clsenc','') + '.txt'
