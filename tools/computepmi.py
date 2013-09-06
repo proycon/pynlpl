ca031092f9e745a5ad4dc3fc98c3f1a498296789
Maarten van Gompel <proycon@anaproy.nl>
2013-09-06 13:55:36 +0200

    fix

--- tools/computepmi.py
+++ tools/computepmi.py
@@ -23,10 +23,10 @@ def npmi(sentences1, sentences2):
 def main():
     parser = argparse.ArgumentParser(description="Simple cooccurence computation", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-f','--inputtext', type=str,help="Input file (plaintext, tokenised, utf-8, one sentence per line)", action='store',default="",required=True)
-    parser.add_argument('-u','--unidirectional', type=bool,help="Compute unidirectionally, i.e. the left word will always occur before the right word", action='store_true',default=False)
-    parser.add_argument('-s','--sorted', type=bool,help="Output sorted by co-occurrence score", action='store_true',default=False)
-    parser.add_argument('--pmi',type=bool,help="Compute pointwise mutual information", action='store_true',default=False)
-    parser.add_argument('--npmi',type=bool,help="Compute normalised pointwise mutual information", action='store_true',default=False)
+    parser.add_argument('-u','--unidirectional', help="Compute unidirectionally, i.e. the left word will always occur before the right word", action='store_true',default=False)
+    parser.add_argument('-s','--sorted', help="Output sorted by co-occurrence score", action='store_true',default=False)
+    parser.add_argument('--pmi',help="Compute pointwise mutual information", action='store_true',default=False)
+    parser.add_argument('--npmi',help="Compute normalised pointwise mutual information", action='store_true',default=False)
 
     args = parser.parse_args()
     if not args.pmi and not args.npmi:
@@ -40,7 +40,7 @@ def main():
         sentence = i + 1
         print("Indexing @" + str(sentence),file=sys.stderr)
         if line:
-            for pos, word in line.split():
+            for pos, word in enumerate(line.split()):
                 if word:
                     index[(pos,word)].add(sentence)
                     reverseindex[sentence].add((pos,word))
@@ -51,17 +51,18 @@ def main():
     l = len(index)
     for i, (pos, word) in enumerate(index):
         print("Computing cooc @" + str(i+1) + "/" + str(l),file=sys.stderr)
-        for pos2, word2 in index[sentence]:
-            if (pos != pos2) or (word != word2):
-                if args.pmi:
-                    score = pmi(index[(pos,word)], index[(pos2,word2)])
-                elif args.npmi:
-                    score = npmi(index[(pos,word)], index[(pos2,word2)])
-                if not (score is None):
-                    if args.sorted:
-                        output.append(word,word2,score)
-                    else:
-                        print(word + "\t" + word2 + "\t" + str(score))
+        for sentence in index[(pos,word)]:
+            for pos2, word2 in reverseindex[sentence]:
+                if (pos != pos2) or (word != word2):
+                    if args.pmi:
+                        score = pmi(index[(pos,word)], index[(pos2,word2)])
+                    elif args.npmi:
+                        score = npmi(index[(pos,word)], index[(pos2,word2)])
+                    if not (score is None):
+                        if args.sorted:
+                            output.append(word,word2,score)
+                        else:
+                            print(word + "\t" + word2 + "\t" + str(score))
 
     if args.sorted:
         for word,word2,score in sorted(output, key=lambda x: -1* x[2]):
