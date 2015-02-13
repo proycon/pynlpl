795ea4a7a0010ca4789628a124ef557953ce2ec0
Maarten van Gompel <proycon@anaproy.nl>
2015-02-13 14:53:31 +0100

    working on FQL parser

--- formats/fql.py
+++ formats/fql.py
@@ -15,24 +15,40 @@
 #----------------------------------------------------------------
 
 from pynlpl.formats import folia
+import re
 
 OPERATORS = ('=','==','!=','>','<','<=','>=')
+MASK_NORMAL = 0
+MASK_LITERAL = 1
+MASK_EXPRESSION = 2
 
-def query(doc, query):
 
+def query(doc, q):
+    q = Query.parse(UnparsedQuery(q))
+    return q(doc)
 
 class SyntaxError(Exception):
     pass
-
-
 
 class UnparsedQuery(object):
     """This class takes care of handling grouped blocks in parentheses and handling quoted values"""
     def __init__(self, s, i):
         self.q = []
+        self.mask = []
         l = len(s)
+        begin = 0
         while i < l:
             c = s[i]
+            if c == " ":
+                #process previous word
+                if begin < i:
+                    self.q.append(s[begin:i])
+                begin = i + 1
+            elif i == l - 1:
+                #process last word
+                self.q.append(s[begin:])
+                self.mask.append(MASK_NORMAL)
+
             if c == '(': #groups
                 #find end quote and process block
                 level = 0
@@ -54,7 +70,9 @@ class UnparsedQuery(object):
                                 level -= 1
                 if s2:
                     self.q.append(UnparsedQuery(s2))
+                    self.mask.append(MASK_EXPRESSION)
                     i = j
+                    begin = i+1
                 else:
                     raise SyntaxError("Unmatched parenthesis at char " + str(i))
             elif c == '"': #literals
@@ -69,9 +87,12 @@ class UnparsedQuery(object):
                                 break
                     if not s2 is None:
                         self.q.append(s2)
+                        self.mask.append(MASK_LITERAL)
                         i = j
+                        begin = i+1
                     else:
                         raise SyntaxError("Unterminated string literal at char " + str(i))
+
             i += 1
 
 
@@ -85,26 +106,21 @@ class UnparsedQuery(object):
     def __getitem__(self, index):
         try:
             return self.q[index]
-        raise KeyError:
+        except KeyError:
             return ""
 
-
-
-
-class Query(object):
-    def __init__(self, s):
-        action =
-        action = Action(s)
-
-
-    def __add__(self, other):
-        if isinstance(other, Query):
-
+    def kw(self, index, value):
+        if isinstance(value, tuple):
+            return self.q[index] in value and self.mask[index] == MASK_NORMAL
         else:
+            return self.q[index] == value and self.mask[index] == MASK_NORMAL
+
+    def __setitem__(self, index, value):
+        self.q[index] = value
 
 
-class DocumentSelector(object):
-    def __init__(self):
+
+
 
 
 class Actor(object):
@@ -128,13 +144,13 @@ class Actor(object):
         id = None
         filter = None
         while i < l:
-            if q[i] == "OF" and q[i+1]:
+            if q.kw(i,"OF") and q[i+1]:
                 set = q[i+1]
                 i += 2
-            elif q[i] == "ID" and q[i+1]:
+            elif q.kw(i,"ID") and q[i+1]:
                 id = q[i+1]
                 i += 2
-            elif q[i] == "WHERE":
+            elif q.kw(i, "WHERE"):
                 #ok, big filter coming up!
                 filter, i = Filter.parse(Filter.parse(),i)
                 break
@@ -158,38 +174,48 @@ class Filter(object): #WHERE ....
 
         l = len(q)
         while i < l:
-            if q[i] == "NOT"
+            if q.kw(i, "NOT"):
                 negation = True
             elif q[i+1] in OPERATORS and q[i] and q[i+2]:
                 operator = q[i+1]
+                if q[i] == "class":
+                    v = lambda x,y='cls': getattr(x,y)
+                elif q[i] == "text":
+                    v = lambda x,y='text': getattr(x,'text')()
+                else:
+                    v = lambda x,y=q[i]: getattr(x,y)
                 if operator == '=' or operator == '==':
-                    self.filters.append( lambda x: getattr(x, q[i]) == q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) == y )
                 elif operator == '!=':
-                    self.filters.append( lambda x: getattr(x, q[i]) != q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) != y )
                 elif operator == '>':
-                    self.filters.append( lambda x: getattr(x, q[i]) > q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) > y )
                 elif operator == '<':
-                    self.filters.append( lambda x: getattr(x, q[i]) < q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) < y )
                 elif operator == '>=':
-                    self.filters.append( lambda x: getattr(x, q[i]) >= q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) >= y )
                 elif operator == '<=':
-                    self.filters.append( lambda x: getattr(x, q[i]) <= q[i+2] )
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x) <= y )
+                elif operator == 'CONTAINS':
+                    self.filters.append( lambda x,y=q[i+2],v=v : v(x).find( y ) != -1 )
+                elif operator == 'MATCHES':
+                    self.filters.append( lambda x,y=re.compile(q[i+2]),v=v : y.search(v(x)) is not None  )
 
-                if q[i+3] == "AND" or q[i+3] == "OR":
+                if q.kw(i+3,"AND") or q.kw(i+3,"OR"):
                     if logop and q[i+3] != logop:
                         raise SyntaxError("Mixed logical operators, use parentheses")
                     logop = q[i+3]
                 else:
-                    break
+                    break #done
             elif isinstance(q, UnparsedQuery):
                 filter, i = Filter.parse(q,i)
                 filters.append(filter)
-                if q[i] == "AND" or q[i] == "OR":
+                if q.kw(i,"AND") or q.kw(i, "OR"):
                     if logop and q[i] != logop:
                         raise SyntaxError("Mixed logical operators, use parentheses")
                     logop = q[i]
                 else:
-                    break
+                    break #done
             else:
                 raise SyntaxError("Expected comparison operator, got " + q[i+1])
             i += 1
@@ -197,67 +223,133 @@ class Filter(object): #WHERE ....
         return Filter(filters, negation, disjunction), i
 
 
+class Target(object):
+    pass #TODO
+
+class Form(Object):  #AS... expression
+    pass #TODO
 
 
-
-
-
-
-
-class Assignment(object):
-    def __init__(self, assignment):
-        self.assignment = assignment
-
-
-
-
-
-
-
-
-
-
-
-class Action(object):
-    def __init__(self, action, actor, assignments = None, targets = None):
-        if action in ('SELECT','EDIT','DELETE','ADD'):
-            self.action = action
-        else:
-            raise SyntaxError("Excepted action: " + actor)
-
+class Action(object): #Action
+    def __init__(self, action, actor, assignments={}):
+        self.action = action
         self.actor = actor
-
-        if self.action in ( 'SELECT','DELETE') and assignments:
-            raise SyntaxError("No WITH statement expected for " + self.action + " action")
-
-        if self.action == 'ADD' and self.actor.filters:
-            raise SyntaxError("No WHERE statement expected for " + self.action + " action")
-
-        if self.action == 'SELECT' and assignments:
-            raise SyntaxError("No WITH statement expected for SELECT action")
-
         self.assignments = assignments
-        self.targets = targets
+        self.form = None
+        self.subactions = []
+        self.nextaction = None
+        self.respan = []
 
 
     @staticmethod
     def parse(q,i=0):
+        if q.kw(i, ('SELECT','EDIT','DELETE','ADD','APPEND','PREPEND','MERGE','SPLIT')):
+           action = q[i]
+        else:
+            raise SyntaxError("Expected action, got " + q[i])
+
         try:
-            s = Actor.parse(s)
+            actor, i = Actor.parse(q,i)
         except ParseError as e:
             raise SyntaxError(e)
 
+        if action == "ADD" and actor.filters:
+            raise SyntaxError("Actor has WHERE statement but ADD action does not support this")
+
+        assignments = {}
+        if q.kw(i,"WITH"):
+            if action in ("SELECT", "DELETE"):
+                raise SyntaxError("Actor has WITH statement but " + action + " does not support this")
+            i += 1
+            l = len(q)
+            while i < l:
+                if q.kw(i, ('annotator','annotatortype','class','confidence','n','text')):
+                    assignments[q[i]] = q[i+1]
+                    i+=1
+                else:
+                    if not assignments:
+                        raise SyntaxError("Expected assignments after WITH statement, but no valid attribute found")
+                    break
+            i+=1
+
+        #we have enough to set up the action now
+        action = Actions(action, actor, assignments)
+
+        if action.action == "EDIT" and q.kw(i,"SPAN"):
+            pass #TODO
+
+        done = False
+        while not done:
+            if isinstance(q[i], UnparsedQuery):
+                #we have a sub expression
+                if q.kw(i, ('EDIT','DELETE','ADD')):
+                    #It's a sub-action!
+                    subaction, _ = Action.parse(q[i])
+                    action.subactions.append( subaction )
+                elif q.kw(i, 'AS'):
+                    #It's an AS.. expression
+                    pass #self.form = #TODO
+            else:
+                done = True
+            i += 1
+
+        if q.kw(i, ('SELECT','EDIT','DELETE','ADD','APPEND','PREPEND','MERGE','SPLIT')):
+            #We have another action!
+            action.nextaction, i = Action.parse(q,i)
+
+        return action, i
 
 
 
-class TargetExpression(object): #FOR .....
-    def __init__(self, ):
 
 
 
-class AssignmentExpression(object): #WITH .....
-    def __init__(self, assignments):
-        self.assignments = assignments
+
+
+
+class Context(object):
+    def __init__(self):
+        self.format = "python"
+        self.returntype = "actor"
+        self.request = "all"
+        self.defaults = {}
+        self.defaultsets = {}
+
+class Query(object):
+    def __init__(self, q, context):
+        self.action = None
+        self.targets = []
+        self.format = context.format
+        self.returntype = context.returntype
+        self.request = copy(context.request)
+        self.defaults = copy(context.defaults)
+        self.defaultsets = copy(context.defaultsets)
+
+    def parse(self, q, i=0):
+        l = len(q)
+        self.action,i = Action.parse(q,i)
+
+        if q.kw(i,("FOR","IN")):
+            self.targets,i = Target.parse(q,i)
+
+        while i < l:
+            if q.kw(i,"RETURN"):
+                pass #TODO
+            elif q.kw(i,"FORMAT"):
+                pass #TODO
+            elif q.kw(i,"REQUEST"):
+                pass #TODO
+
+        if i != l:
+            raise SyntaxError("Expected end of query, got " + q[i])
+
+    def __call__(self, doc):
+        """Execute the query on the specified document"""
+        pass #TODO
+
+
+
+
 
 
 
