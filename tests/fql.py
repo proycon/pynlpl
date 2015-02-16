a55a8beb07442a606eac243fe89bc4f62019a59e
Maarten van Gompel <proycon@anaproy.nl>
2015-02-16 21:41:39 +0100

    added test for EDIT with fallback to ADD

--- tests/fql.py
+++ tests/fql.py
@@ -43,6 +43,7 @@ Qselect_nestedtargets = "SELECT lemma OF \"lemmas-nl\" WHERE class = \"stamboom\
 
 Qedit = "EDIT lemma OF \"lemmas-nl\" WHERE class = \"stamboom\" WITH class \"blah\" FOR w FOR s ID \"WR-P-E-J-0000000001.p.1.s.2\""
 Qadd = "ADD lemma OF \"lemmas-nl\" WITH class \"hebben\" FOR w ID \"WR-P-E-J-0000000001.sandbox.2.s.1.w.3\""
+Qeditadd = "EDIT lemma OF \"lemmas-nl\" WITH class \"hebben\" FOR w ID \"WR-P-E-J-0000000001.sandbox.2.s.1.w.3\""
 
 class Test1UnparsedQuery(unittest.TestCase):
 
@@ -117,6 +118,11 @@ class Test3Evaluation(unittest.TestCase):
 
     def test6_evaluate_add(self):
         q = fql.Query(Qadd)
+        results = q(self.doc)
+        self.assertTrue(isinstance(results[0], folia.LemmaAnnotation))
+
+    def test6_evaluate_editadd(self):
+        q = fql.Query(Qeditadd)
         results = q(self.doc)
         self.assertTrue(isinstance(results[0], folia.LemmaAnnotation))
 
