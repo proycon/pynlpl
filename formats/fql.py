26b428274a17e9b75db0662c506d9740cf4b266c
Maarten van Gompel <proycon@anaproy.nl>
2015-02-20 12:17:58 +0100

    the future is now

--- formats/fql.py
+++ formats/fql.py
@@ -14,6 +14,12 @@
 #
 #----------------------------------------------------------------
 
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
 from pynlpl.formats import folia
 from copy import copy
 import json
