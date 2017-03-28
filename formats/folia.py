2a072f9b56eb5264d4c48419f98d3b7e5b6f1214
Maarten van Gompel <proycon@anaproy.nl>
2017-03-28 12:30:02 +0200

    regenerated foliaspec again

--- formats/folia.py
+++ formats/folia.py
@@ -7584,7 +7584,7 @@ def validate(filename,schema=None,deep=False):
 #================================= FOLIA SPECIFICATION ==========================================================
 
 #foliaspec:header
-#This file was last updated according to the FoLiA specification for version 1.4.1 on 2017-03-28 12:25:22, using foliaspec.py
+#This file was last updated according to the FoLiA specification for version 1.4.1 on 2017-03-28 12:29:59, using foliaspec.py
 #Code blocks after a foliaspec comment (until the next newline) are automatically generated. **DO NOT EDIT THOSE** and **DO NOT REMOVE ANY FOLIASPEC COMMENTS** !!!
 
 #foliaspec:structurescope:STRUCTURESCOPE
